ca68ffeb82055bebe2cfa06cc75fee5f1d79da51
Fraccas <daltonward1@yahoo.com>
2019-10-11 13:10:33 -0500

    Finished login/register

--- src/server/routes/auth/login.ts
+++ src/server/routes/auth/login.ts
@@ -1,7 +1,27 @@
-import { Router } from 'express';
+import * as express from 'express';
+import * as passport from 'passport';
 
+import { CreateToken } from '../../utils/security/tokens';
+import { any } from 'prop-types';
 
-const router = Router();
+const router = express.Router();
+
+router.post('/', passport.authenticate('local'), async (req: any, res, next) => {
+    try {
+        console.log("BookStore Attempting Login: " + req.user.email);
+        let token = await CreateToken({ userid: req.user.id });
+        res.json({
+            token,
+            role: req.user.role,
+            userid: req.user.id
+        });
+        console.log("Login Success...");
+    } catch (e) {
+        console.log("Login Failed...")
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
 
 
 export default router;
--- src/server/routes/auth/register.ts
+++ src/server/routes/auth/register.ts
@@ -1,7 +1,28 @@
-import { Router } from 'express';
+import * as express from 'express';
+import DB from '../../db';
 
+import { HashPass } from '../../utils/security/password';
+import { CreateToken } from '../../utils/security/tokens';
 
-const router = Router();
+const router = express.Router();
 
+router.post('/', async (req, res, next) => {
+    try {
+        let user = req.body;
+        console.log("Bookstore Registering new user: " + user.name);
+        user.password = HashPass(req.body.password);
+        let result: any = await DB.Users.createUser(user.name, user.email, user.password);
+        let token = await CreateToken({ userid: result.insertId });
+        res.json({
+            token, 
+            role: 'guest',
+            userid: result.insertId
+        });
+        console.log(user.name + " has been added as a new user!");
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500); 
+    }
+});
 
 export default router;
